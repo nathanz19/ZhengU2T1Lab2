82b3c4140247534abab949ab76968028b96d6dae
Nathan Zheng <nathanz@nycstudents.net>
2022-09-30 08:25:50 -0400

    Upgrade

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,14 @@
 public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle1 = new Circle(3.43);
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle1.printArea();
+        // C. BEFORE running this code, PREDICT what gets printed
+        //> My area is: 36.941786
+        // D. run the code and confirm your prediction! Were you correct?
+        //I was correct with my prediction but the decimal value was a bit larger than my prediction.
+    }
 }
+
